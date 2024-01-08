7a29cca48b3676c3020032a09df7a3406def0483
Gengsu07 <sugengdatascience@gmail.com>
2024-01-08 17:56:53 +0700

    o

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,10 @@
-import SideBar from "@/app/dashboard/Sidebar";
-import Header from "@/components/Header";
-import MainDashboard from "./MainDashboard";
-import NavBar from "@/components/NavBar";
-import { Separator } from "@/components/ui/separator";
+import KPI from "./dashboard_component/kpi";
 
 const Dashboard = () => {
   return (
     <main className="w-screen h-screen ">
       <div className="flex  justify-center items-center ">
-        <MainDashboard />
+        <KPI />
       </div>
     </main>
   );
