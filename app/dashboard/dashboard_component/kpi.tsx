799c645cc7910a987989c569bef7f375d72b3307
Sugeng Wahyudi <sugengdatascience@gmail.com>
2024-01-15 00:25:56 +0700

    done:KPI

--- app/dashboard/dashboard_component/kpi.tsx
+++ app/dashboard/dashboard_component/kpi.tsx
@@ -1,49 +1,66 @@
 "use client";
 import useFilterData from "@/app/store/useFilterData";
 import querystring from "querystring";
-
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { TiMinus, TiPlus } from "react-icons/ti";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useQuery } from "@tanstack/react-query";
+import { TResponseData } from "@/app/api/kpi/route";
 
 const KPI = () => {
   const { filterData, parseFilterData } = useFilterData();
   const cleanFilterData = parseFilterData(filterData) || {};
-  console.log("kpi:", cleanFilterData);
   const queryParamsString = querystring.stringify(cleanFilterData);
 
-  const { data, isFetching, error } = useQuery({
-    queryKey: ["kpi"],
+  const { data, isFetching, error } = useQuery<TResponseData[]>({
+    queryKey: [queryParamsString],
     queryFn: () =>
-      fetch("http://localhost:3000/api/kpi?" + queryParamsString).then((res) =>
-        res.json()
-      ),
+      fetch("http://localhost:3000/api/kpi?" + queryParamsString, {
+        cache: "no-store",
+      }).then((res) => res.json()),
   });
-  console.log("data KPI:", data);
+
+  // const { netto, bruto, restitusi } = data || {};
+
+  const convertNominal = (nominal: number) => {
+    if (nominal > 1000000000000)
+      return `${(nominal / 1000000000000).toFixed(2)} T`;
+    if (nominal / 1000000000 === 1 || -1)
+      return `${(nominal / 1000000000).toFixed(2)} M`;
+    if (nominal / 1000000 == 1 || -1)
+      return `${(nominal / 1000000).toFixed(2)} Jt`;
+  };
 
   return (
-    <section className="flex flex-col justify-center items-center gap-1  w-full h-full mx-auto bg-background-primary">
-      <Card>
-        <CardHeader>
-          <CardTitle>Netto</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <p className="text-xl">{data._sum.nominal}</p>
-        </CardContent>
-        <CardFooter>
-          <p>Card Footer</p>
-        </CardFooter>
-      </Card>
+    <section className="flex flex-col sm:flex-row justify-around items-start gap-5  w-full h-full mx-auto  bg-background-primary">
+      {data?.map((item) => (
+        <Card className="w-full" key={item.label}>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">{item.label}</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className=" flex justify-between items-center flex-nowrap gap-5">
+              <p className="text-xl md:text-2xl font-bold">
+                {convertNominal(item.value.cy)}
+              </p>
+              <div className="flex justify-start items-center space-x-2">
+                {item?.label === "Restitusi" ? (
+                  item.value.yoy > 0 ? (
+                    <TiPlus color="red" />
+                  ) : (
+                    <TiMinus color="green" />
+                  )
+                ) : item.value.yoy > 0 ? (
+                  <TiPlus color="green" />
+                ) : (
+                  <TiMinus color="red" />
+                )}
 
-      <div className="flex justify-center items-center w-full h-full ">
-        <p>sdfsd</p>
-      </div>
+                <p>{`${item.value.yoy.toFixed(2)}%`}</p>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+      ))}
     </section>
   );
 };
