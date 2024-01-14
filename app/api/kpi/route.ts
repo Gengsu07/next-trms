25df0b837e76892a45be5a215872500bd6224140
Sugeng Wahyudi <sugengdatascience@gmail.com>
2024-01-14 12:42:33 +0700

    now

--- app/api/kpi/route.ts
+++ app/api/kpi/route.ts
@@ -11,7 +11,7 @@ export async function GET(req: NextRequest, res: NextResponse) {
   const npwpValues = searchParams.getAll("npwp");
 
   const filterConditions: Record<string, any> = {
-    from: fromValue,
+    from: fromValue?.toString(),
     to: toValue,
     sektor: sektorValues.length > 0 ? { in: sektorValues } : undefined,
     admin: adminValues.length > 0 ? { in: adminValues } : undefined,
@@ -23,14 +23,13 @@ export async function GET(req: NextRequest, res: NextResponse) {
   const cleanFilterConditions = Object.fromEntries(
     Object.entries(filterConditions).filter(([_, value]) => value !== undefined)
   );
-
+  console.log(cleanFilterConditions);
   //query db
-  const data = await prisma.mpn.aggregate({
-    _sum: {
-      nominal: true,
-    },
+  const data = await prisma.mpn.findMany({
     where: {
-     
+      datebayar: {
+        gte: "2023-01-01",
+      },
     },
   });
   return NextResponse.json(data);
