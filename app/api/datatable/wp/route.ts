66cd2f7d62a51994b16bf2936706cb580c7a14bd
Sugeng Wahyudi <sugengdatascience@gmail.com>
2024-02-19 22:16:26 +0700

    done :API per WP-datatable using QUERYRAW

--- app/api/datatable/wp/route.ts
+++ app/api/datatable/wp/route.ts
@@ -1,8 +1,11 @@
+import { sektor } from "./../../../../constant/initialData";
 import { format } from "date-fns";
 import { prisma } from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import { Prisma } from "@prisma/client";
 import { buildWhereClause } from "@/lib/utils";
+import { DataFrame } from "data-forge";
+import dataforgefs from "data-forge-fs";
 
 export async function GET(req: NextRequest, res: NextResponse) {
   const searchParams = req.nextUrl.searchParams;
@@ -29,7 +32,47 @@ export async function GET(req: NextRequest, res: NextResponse) {
     Object.entries(filterConditions).filter(([_, value]) => value !== undefined)
   );
 
-  const [where_cy, wherevalues_cy] = buildWhereClause(cleanFilterConditions);
+  const currentPage = 1; // Or the desired page number
+  const perPage = 10; // Or the desired number of items per page
+  const offset = (currentPage - 1) * perPage;
+
+  const filterSektor =
+    cleanFilterConditions.sektor &&
+    `kd_kategori IN (${cleanFilterConditions.sektor.in
+      .map((v: string) => `'${v}'`)
+      .join(", ")})`;
+  const filterMap =
+    cleanFilterConditions.map &&
+    `kdmap IN (${cleanFilterConditions.map.in
+      .map((v: string) => `'${v}'`)
+      .join(", ")})`;
+  const filterAdmin =
+    cleanFilterConditions.admin &&
+    `admin IN (${cleanFilterConditions.admin.in
+      .map((v: string) => `'${v}'`)
+      .join(", ")})`;
+  const filterKjs =
+    cleanFilterConditions.kjs &&
+    `kdbayar IN (${cleanFilterConditions.kjs.in
+      .map((v: string) => `'${v}'`)
+      .join(", ")})`;
+  const filterNpwp =
+    cleanFilterConditions.npwp &&
+    `npwp15 IN (${cleanFilterConditions.npwp.in
+      .map((v: string) => `'${v}'`)
+      .join(", ")})`;
+
+  const AllFilter = [
+    filterSektor,
+    filterMap,
+    filterAdmin,
+    filterKjs,
+    filterNpwp,
+  ]
+    .filter(Boolean)
+    .join(" AND ");
+
+  const whereClause = AllFilter ? `WHERE ${AllFilter}` : "";
 
   const from = new Date(cleanFilterConditions.from);
   const to = new Date(cleanFilterConditions.to);
@@ -40,104 +83,113 @@ export async function GET(req: NextRequest, res: NextResponse) {
     from.getDate()
   );
   const PY_to = new Date(to.getFullYear() - 1, to.getMonth(), to.getDate());
-
-  const currentPage = 1; // Or the desired page number
-  const perPage = 10; // Or the desired number of items per page
-  const offset = (currentPage - 1) * perPage;
-
-  const cy_netto = await prisma.$queryRaw(
+  const perwp = await prisma.$queryRaw(
     Prisma.sql`SELECT
     m.npwp15,
     m.nama_wp,
-    sum(CASE WHEN m.datebayar BETWEEN ${new Date(
+    sum(CASE WHEN m.datebayar >= ${new Date(
       cleanFilterConditions.from
-    )} AND ${new Date(cleanFilterConditions.to)} THEN m.nominal END) AS "cy",
-    sum(CASE WHEN m.datebayar BETWEEN ${PY_from} AND ${PY_to} THEN m.nominal END) AS "py"
+    )} AND m.datebayar <= ${new Date(
+      cleanFilterConditions.to
+    )}  THEN m.nominal END) AS "netto_cy",
+    sum(CASE WHEN m.datebayar >= ${new Date(
+      cleanFilterConditions.from
+    )} AND m.datebayar <= ${new Date(
+      cleanFilterConditions.to
+    )} and ket IN ('MPN','SPM')  THEN m.nominal END) AS "bruto_cy",
+    sum(CASE WHEN m.datebayar >= ${PY_from} AND m.datebayar <= ${PY_to} THEN m.nominal END) AS "netto_py",
+    sum(CASE WHEN m.datebayar >= ${PY_from} AND m.datebayar <= ${PY_to} and  ket IN ('MPN','SPM') THEN m.nominal END) AS "bruto_py"
     FROM mpn m
-    WHERE 
-    GROUP BY m.npwp15, m.nama_wp
+    ${Prisma.raw(whereClause)}
+    GROUP BY m.npwp15, m.nama_wp 
+    ORDER BY sum(CASE WHEN m.datebayar >= ${new Date(
+      cleanFilterConditions.from
+    )} AND m.datebayar <= ${new Date(
+      cleanFilterConditions.to
+    )}  THEN m.nominal END) DESC NULLS LAST
     LIMIT ${perPage}
-    OFFSET ${offset};`
+    OFFSET ${offset};
+   `
   );
 
-  //   await prisma.mpn.groupBy({
-  //     by: ["npwp15", "nama_wp"],
-  //     _sum: {
-  //       nominal: true,
+  // const cy_netto = await prisma.mpn.groupBy({
+  //   by: ["npwp15", "nama_wp"],
+  //   _sum: {
+  //     nominal: true,
+  //   },
+  //   where: {
+  //     datebayar: {
+  //       gte: new Date(cleanFilterConditions.from),
+  //       lte: new Date(cleanFilterConditions.to),
   //     },
-  //     where: {
-  //       datebayar: {
-  //         gte: new Date(cleanFilterConditions.from),
-  //         lte: new Date(cleanFilterConditions.to),
+  //     ...(cleanFilterConditions.sektor && {
+  //       kd_kategori: {
+  //         in: cleanFilterConditions.sektor.in,
   //       },
-  //       ...(cleanFilterConditions.sektor && {
-  //         kd_kategori: {
-  //           in: cleanFilterConditions.sektor.in,
-  //         },
-  //       }),
-  //       ...(cleanFilterConditions.map && {
-  //         kdmap: {
-  //           in: cleanFilterConditions.map.in,
-  //         },
-  //       }),
-  //       ...(cleanFilterConditions.admin && {
-  //         admin: {
-  //           in: cleanFilterConditions.admin.in,
-  //         },
-  //       }),
-  //       ...(cleanFilterConditions.kjs && {
-  //         kdbayar: {
-  //           in: cleanFilterConditions.kjs.in,
-  //         },
-  //       }),
-  //       ...(cleanFilterConditions.npwp && {
-  //         npwp15: {
-  //           in: cleanFilterConditions.npwp.in,
-  //         },
-  //       }),
-  //     },
-  //   });
+  //     }),
+  //     ...(cleanFilterConditions.map && {
+  //       kdmap: {
+  //         in: cleanFilterConditions.map.in,
+  //       },
+  //     }),
+  //     ...(cleanFilterConditions.admin && {
+  //       admin: {
+  //         in: cleanFilterConditions.admin.in,
+  //       },
+  //     }),
+  //     ...(cleanFilterConditions.kjs && {
+  //       kdbayar: {
+  //         in: cleanFilterConditions.kjs.in,
+  //       },
+  //     }),
+  //     ...(cleanFilterConditions.npwp && {
+  //       npwp15: {
+  //         in: cleanFilterConditions.npwp.in,
+  //       },
+  //     }),
+  //   },
+  // });
 
-  //   const cy_bruto = await prisma.mpn.groupBy({
-  //     by: ["npwp15", "nama_wp"],
-  //     _sum: {
-  //       nominal: true,
+  // const cy_bruto = await prisma.mpn.groupBy({
+  //   by: ["npwp15", "nama_wp"],
+  //   _sum: {
+  //     nominal: true,
+  //   },
+  //   where: {
+  //     datebayar: {
+  //       gte: new Date(cleanFilterConditions.from),
+  //       lte: new Date(cleanFilterConditions.to),
   //     },
-  //     where: {
-  //       datebayar: {
-  //         gte: new Date(cleanFilterConditions.from),
-  //         lte: new Date(cleanFilterConditions.to),
-  //       },
-  //       ket: {
-  //         in: ["MPN", "SPM"],
-  //       },
-  //       ...(cleanFilterConditions.sektor && {
-  //         kd_kategori: {
-  //           in: cleanFilterConditions.sektor.in,
-  //         },
-  //       }),
-  //       ...(cleanFilterConditions.map && {
-  //         kdmap: {
-  //           in: cleanFilterConditions.map.in,
-  //         },
-  //       }),
-  //       ...(cleanFilterConditions.admin && {
-  //         admin: {
-  //           in: cleanFilterConditions.admin.in,
-  //         },
-  //       }),
-  //       ...(cleanFilterConditions.kjs && {
-  //         kdbayar: {
-  //           in: cleanFilterConditions.kjs.in,
-  //         },
-  //       }),
-  //       ...(cleanFilterConditions.npwp && {
-  //         npwp15: {
-  //           in: cleanFilterConditions.npwp.in,
-  //         },
-  //       }),
+  //     ket: {
+  //       in: ["MPN", "SPM"],
   //     },
-  //   });
+  //     ...(cleanFilterConditions.sektor && {
+  //       kd_kategori: {
+  //         in: cleanFilterConditions.sektor.in,
+  //       },
+  //     }),
+  //     ...(cleanFilterConditions.map && {
+  //       kdmap: {
+  //         in: cleanFilterConditions.map.in,
+  //       },
+  //     }),
+  //     ...(cleanFilterConditions.admin && {
+  //       admin: {
+  //         in: cleanFilterConditions.admin.in,
+  //       },
+  //     }),
+  //     ...(cleanFilterConditions.kjs && {
+  //       kdbayar: {
+  //         in: cleanFilterConditions.kjs.in,
+  //       },
+  //     }),
+  //     ...(cleanFilterConditions.npwp && {
+  //       npwp15: {
+  //         in: cleanFilterConditions.npwp.in,
+  //       },
+  //     }),
+  //   },
+  // });
 
   //   const from = new Date(cleanFilterConditions.from);
   //   const to = new Date(cleanFilterConditions.to);
@@ -228,15 +280,36 @@ export async function GET(req: NextRequest, res: NextResponse) {
   //     },
   //   });
 
-  // const naik_netto =cy_netto.?.map(())
+  // // const naik_netto =cy_netto.?.map(())
 
-  // (cy_netto?._sum?.nominal ?? 0) - (py_netto?._sum?.nominal ?? 0);
-  // const yoy_netto =
-  //   (naik_netto / (py_netto?._sum?.nominal ?? naik_netto)) * 100;
-  // const naik_bruto =
-  //   (cy_bruto?._sum?.nominal ?? 0) - (py_bruto?._sum?.nominal ?? 0);
-  // const yoy_bruto =
-  //   (naik_bruto / (py_bruto?._sum?.nominal ?? naik_bruto)) * 100;
+  // // (cy_netto?._sum?.nominal ?? 0) - (py_netto?._sum?.nominal ?? 0);
+  // // const yoy_netto =
+  // //   (naik_netto / (py_netto?._sum?.nominal ?? naik_netto)) * 100;
+  // // const naik_bruto =
+  // //   (cy_bruto?._sum?.nominal ?? 0) - (py_bruto?._sum?.nominal ?? 0);
+  // // const yoy_bruto =
+  // //   (naik_bruto / (py_bruto?._sum?.nominal ?? naik_bruto)) * 100;
 
-  return NextResponse.json(cy_netto);
+  // const df_cy_netto = new DataFrame(cy_netto);
+  // const df_py_netto = new DataFrame(py_netto);
+  // const df_cy_bruto = new DataFrame(cy_bruto);
+  // const df_py_bruto = new DataFrame(py_bruto);
+
+  // // Merge the two dataframes on 'npwp15' and 'nama_wp'
+  // const merged_netto = df_cy_netto.joinOuter(
+  //   df_py_netto,
+  //   (leftRow) => leftRow.npwp15,
+  //   (rightRow) => rightRow.npwp15,
+  //   (leftRow, rightRow) => ({
+  //     npwp15: leftRow ? leftRow.npwp15 : rightRow.npwp15,
+  //     nama_wp: leftRow ? leftRow.nama_wp : rightRow.nama_wp,
+  //     cy : leftRow ? leftRow?._sum.nominal : null,
+  //     py: rightRow ? rightRow?._sum_nominal : null,
+  //     naik_netto: (leftRow?._sum?.nominal || 0) - (rightRow?._sum.nominal || 0),
+  //   })
+  // );
+
+  // // Extract the merged data as an array
+  // const mergedData = merged_df.toArray();
+  return NextResponse.json(perwp);
 }
