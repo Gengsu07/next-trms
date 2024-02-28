8b53509ccda2d4b2a07fcf39bb70f910b5a454f4
Sugeng Wahyudi <sugengdatascience@gmail.com>
2024-02-28 07:19:21 +0700

    add fab_filterdata

---
+++ components/fab_button.tsx
@@ -0,0 +1,14 @@
+import { LucideBrainCircuit } from "lucide-react";
+import { Button } from "./ui/button";
+
+const FAB_Filter = () => {
+  return (
+    <div className="absolute bottom-4 right-4 bg-accent-foreground rounded-full hover:bg-background  ">
+      <Button className="text-white px-4 w-auto h-12  flex justify-center items-center gap-1 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none rounded-full">
+        <LucideBrainCircuit />
+        <span className="text-sm font-mono">Filter Data</span>
+      </Button>
+    </div>
+  );
+};
+export default FAB_Filter;
